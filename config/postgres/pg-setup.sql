322089a745da304e8300161b62c3e62c2c1e8913
romanDj <true-rock99@ya.ru>
2020-01-11 17:53:11 +0300

    edit docker configs

--- config/postgres/pg-setup.sql
+++ config/postgres/pg-setup.sql
@@ -1,6 +1,6 @@
 CREATE DATABASE dnd_server;
-CREATE USER dndAdmin WITH PASSWORD '123qweASD';
-ALTER ROLE dndAdmin SET client_encoding TO 'utf8';
-ALTER ROLE dndAdmin SET default_transaction_isolation TO 'read committed';
-ALTER ROLE dndAdmin SET timezone TO 'UTC';
-GRANT ALL PRIVILEGES ON DATABASE dnd_server TO dndAdmin;
+CREATE USER dndadmin WITH PASSWORD '123qweASD';
+ALTER ROLE dndadmin SET client_encoding TO 'utf8';
+ALTER ROLE dndadmin SET default_transaction_isolation TO 'read committed';
+ALTER ROLE dndadmin SET timezone TO 'UTC';
+GRANT ALL PRIVILEGES ON DATABASE dnd_server TO dndadmin;
